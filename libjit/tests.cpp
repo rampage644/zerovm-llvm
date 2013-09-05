72f813700dbf0adaa0662e4d6ec14ad5b1900ab4
Sergei Turukin <rampage644@gmail.com>
2013-09-05 16:02:29 +0300

    Tests for libjit

--- libjit/tests.cpp
+++ libjit/tests.cpp
@@ -1,23 +1,39 @@
 #include "gtest/gtest.h"
+#include "jit.h"
+
+const char ExistentFile[] = "/dev/src.cpp";
+const char NonExistentFile[] = "___src___.cpp";
+const char ExistentFunction[] = "function1";
+const char NonExistentFunction[] = "___function1____";
 
 TEST(libjitTest, NonExistentFileTest) {
-  FAIL();
+  EXPECT_EQ(getFunctionPointer(NonExistentFile, ""), (void*)NULL);
 }
 
 TEST(libjitTest, ExistentFileTest) {
-  FAIL();
+  EXPECT_NE(getFunctionPointer(ExistentFile, ExistentFunction), (void*)NULL);
+  EXPECT_EQ(getFunctionPointer(ExistentFile, NonExistentFunction), (void*)NULL);
+  EXPECT_NE(getFunctionPointer(ExistentFile, ExistentFunction), (void*)NULL);
 }
 
 TEST(libjitTest, NonExistentFunctionTest) {
-  FAIL();
+  EXPECT_EQ(getFunctionPointer(ExistentFile, NonExistentFunction), (void*)NULL);
 }
 
 TEST(libjitTest, ExistentFunctionTest) {
-  FAIL();
+  EXPECT_NE(getFunctionPointer(ExistentFile, ExistentFunction), (void*)NULL);
 }
 
 TEST(libjitTest, FunctionRunTest) {
-  FAIL();
+  void* ptr = getFunctionPointer(ExistentFile, ExistentFunction);
+  ASSERT_NE(ptr, (void*)NULL);
+  typedef int (*fptr)(int, int);
+  fptr function = (fptr) ptr;
+  ASSERT_NE(function, (void*)NULL);
+  // function = two int's sum
+  EXPECT_EQ(function(0, 0), 0);
+  EXPECT_EQ(function(1, 5), 6);
+  EXPECT_EQ(function(5, -9), -4);
 }
 
 int main(int argc, char** argv) {
